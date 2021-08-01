72f3624e1a86c09d8b5d2f654df7e9156a33a849
WojciechMadeja <wojciech-madeja@wp.pl>
2021-08-01 18:43:42 +0200

    Added sounds to ArrowAreas

--- src/components/atoms/ButtonStart/ButtonStart.js
+++ src/components/atoms/ButtonStart/ButtonStart.js
@@ -5,17 +5,15 @@ import start from '../../../assets/buttonstart.wav'
 
 const buttonStart = new Howl({
     src: [ start ],
-    autoplay: true,
     volume: 0.7,
 });    
 function ButtonStart({ stateButton, setStateButton }) {
     const clickStartButton = () =>{
         setStateButton(!stateButton);
         buttonStart.play();
-        console.log('click');
     }
     return(
-        <Link to="/game" ><Wrapper stateButton = { stateButton }  onClick= { () => clickStartButton } > START </Wrapper></Link>
+        <Link to="/game" ><Wrapper stateButton = { stateButton }  onClick= { () => clickStartButton() } > START </Wrapper></Link>
     );
 }
 
--- src/components/molecules/LeftAreaButton/LeftAreaButton.js
+++ src/components/molecules/LeftAreaButton/LeftAreaButton.js
@@ -1,24 +1,28 @@
 import ArrowLeft from '../../atoms/ArrowLeft/ArrowLeft';
 import TextQuizAreaLeft from '../../atoms/TextQuizAreaLeft/TextQuizAreaLeft';
 import { Wrapper } from './LeftAreaButton.styles';
-// import {Howl, Howler} from 'howler';
-// import click from '../../../assets/click.mp3'
+import { Howl } from 'howler';
+import trueAnswer from '../../../assets/true.mp3'
+import wrongAnswer from '../../../assets/false.wav'
 
 function LeftAreaButton({ answer, statePoints, setPoints, stateLives, setLives }) {
-    // var sound = new Howl({
-    //     src: [ click ],
-    //     autoplay: true,
-    //     volume: 0.2,
-    //   });
+    const trueArrow = new Howl ({
+        src: [ trueAnswer ],
+        volume: 0.2,
+      });
+      const wrongArrow = new Howl ({
+        src: [ wrongAnswer ],
+        volume: 0.2,
+      });
     const buttonFunction = () => {
         if (answer === true){
             setPoints(statePoints + 1);
-            // sound.once('load', function(){
-            //     sound.play();
-            // });
+            trueArrow.play();
+
         }
         if (answer === false){
             setLives(stateLives - 1);
+            wrongArrow.play();
         }
     }
     return(
