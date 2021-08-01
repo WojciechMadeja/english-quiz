d14abcc9227b43beac14d893c013a6f2a62e191a
WojciechMadeja <wojciech-madeja@wp.pl>
2021-08-01 17:39:11 +0200

    Added sound ButtonStart

--- src/components/atoms/ButtonStart/ButtonStart.js
+++ src/components/atoms/ButtonStart/ButtonStart.js
@@ -1,10 +1,21 @@
 import { Wrapper } from './ButtonStart.styles';
 import { Link } from 'react-router-dom';
+import {Howl, Howler} from 'howler';
+import start from '../../../assets/buttonstart.wav'
 
-
+const buttonStart = new Howl({
+    src: [ start ],
+    autoplay: true,
+    volume: 0.7,
+});    
 function ButtonStart({ stateButton, setStateButton }) {
+    const clickStartButton = () =>{
+        setStateButton(!stateButton);
+        buttonStart.play();
+        console.log('click');
+    }
     return(
-        <Link to="/game" ><Wrapper stateButton = { stateButton }  onClick= { () => setStateButton(!stateButton) } > START </Wrapper></Link>
+        <Link to="/game" ><Wrapper stateButton = { stateButton }  onClick= { () => clickStartButton } > START </Wrapper></Link>
     );
 }
 
--- src/components/molecules/LeftAreaButton/LeftAreaButton.js
+++ src/components/molecules/LeftAreaButton/LeftAreaButton.js
@@ -1,11 +1,21 @@
 import ArrowLeft from '../../atoms/ArrowLeft/ArrowLeft';
 import TextQuizAreaLeft from '../../atoms/TextQuizAreaLeft/TextQuizAreaLeft';
 import { Wrapper } from './LeftAreaButton.styles';
+// import {Howl, Howler} from 'howler';
+// import click from '../../../assets/click.mp3'
 
 function LeftAreaButton({ answer, statePoints, setPoints, stateLives, setLives }) {
+    // var sound = new Howl({
+    //     src: [ click ],
+    //     autoplay: true,
+    //     volume: 0.2,
+    //   });
     const buttonFunction = () => {
         if (answer === true){
             setPoints(statePoints + 1);
+            // sound.once('load', function(){
+            //     sound.play();
+            // });
         }
         if (answer === false){
             setLives(stateLives - 1);
